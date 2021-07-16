d7c7ec9f5eb590fd175e8786957e3866a1f11800
Shelby Neil Smith <ssmith@eezy.com>
2021-07-16 11:34:28 -0500

    avoid self collisions

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.post('/end', handleEnd)
 
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
 
+const possibleMoves = ['up', 'down', 'left', 'right']
 
 function handleIndex(request, response) {
   var battlesnakeInfo = {
@@ -35,12 +36,13 @@ function handleStart(request, response) {
 function handleMove(request, response) {
   var gameData = request.body
 
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  const thisMove = move(gameData)
+  
+  console.log('MOVE: ' + thisMove)
+  console.log('GAME DATA: ',gameData);
 
-  console.log('MOVE: ' + move)
   response.status(200).send({
-    move: move
+    move: thisMove
   })
 }
 
@@ -49,4 +51,91 @@ function handleEnd(request, response) {
 
   console.log('END')
   response.status(200).send('ok')
+}
+
+function move(gameData) {
+  const movesArr = [...possibleMoves]
+
+  console.log('movesArr:', movesArr)
+  console.log('body position: ', gameData.you.body)
+
+  // loop through possible moves to see if we can
+  // find a safe move to make
+  while(movesArr.length) {
+    // Pull a random move off the list of possible moves
+    // (returns array or removed items)
+    const moveDir = movesArr.splice(movesArr.indexOf(randomMove(movesArr)), 1)[0]
+
+    console.log('\n\n** OK, lets try a move! **')
+    console.log('MOVE WE ARE ABOUT TO TRY', moveDir)
+    console.log('MOVE LEFT AFTER THIS ONE', movesArr)
+    
+    if (tryMove({ gameData, moveDir })) {
+      movesArr.length = 0 // empty out temp moves list so we don't keep looping
+      return moveDir // send the safe move back to the app to move our snek
+    }
+  }
+}
+
+function randomMove(movesArr = possibleMoves) {
+  return movesArr[Math.floor(Math.random() * movesArr.length)]
+}
+
+function tryMove({ gameData, moveDir }) {
+  if (hitSelf({ gameData, moveDir })) return false // Do not run into self!
+
+  // No collisions! We can move this direction!
+  return true
+}
+
+function hitSelf({ gameData, moveDir }) {
+  const newHeadPos = updatedHeadCoords({ curHeadCoords: gameData.you.head, moveDir })
+  const bodyCoords = gameData.you.body
+
+  console.log('bodyCoords: ', bodyCoords)
+
+  if(bodyCoords.some(coord => coord.x === newHeadPos.x && coord.y === newHeadPos.y)){
+    console.log('** GONNA HIT YERSELF, HUN! **\n')
+    return true
+  }
+
+  return false
+}
+
+function updatedHeadCoords({ curHeadCoords, moveDir }) {
+  console.log('curHeadCoords: ', curHeadCoords)
+  let newHeadCoords
+
+  switch (moveDir) {
+    case 'up':
+      newHeadCoords = {
+        x: curHeadCoords.x,
+        y: curHeadCoords.y + 1,
+      }
+      break;
+    case 'down':
+      newHeadCoords = {
+        x: curHeadCoords.x,
+        y: curHeadCoords.y - 1,
+      }
+      break;
+    case 'left':
+      newHeadCoords = {
+        x: curHeadCoords.x - 1,
+        y: curHeadCoords.y,
+      }
+      break;
+    case 'right':
+      newHeadCoords = {
+        x: curHeadCoords.x + 1,
+        y: curHeadCoords.y,
+      }
+      break;
+    default:
+      curHeadCoords
+  }
+
+  console.log('test head coords', newHeadCoords)
+
+  return newHeadCoords
 }
